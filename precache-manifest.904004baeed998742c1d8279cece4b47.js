d0077f48ea09cc4b09c75fcc3a3e0beef06c378f
Eduardo Gusmão Filho <42840856+EduardoBBGusmao@users.noreply.github.com>
2020-03-11 18:07:32 -0300

    Add files via upload

---
+++ precache-manifest.904004baeed998742c1d8279cece4b47.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c13e9dcf2a69e1752276e728144984a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b1814705e11b4ac8e3f9",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "46d7eb01ee83ea59f98a",
+    "url": "/static/css/main.d2f0fcc3.chunk.css"
+  },
+  {
+    "revision": "b1814705e11b4ac8e3f9",
+    "url": "/static/js/2.fb212e32.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/static/js/2.fb212e32.chunk.js.LICENSE"
+  },
+  {
+    "revision": "46d7eb01ee83ea59f98a",
+    "url": "/static/js/main.86ce2153.chunk.js"
+  },
+  {
+    "revision": "2989595f2546bfb0b1db",
+    "url": "/static/js/runtime-main.60ebcab2.js"
+  },
+  {
+    "revision": "cef63d6377dfe52a8d779a3dd4c45fb6",
+    "url": "/static/media/LemonMilk.cef63d63.otf"
+  },
+  {
+    "revision": "bb255cd0eddfad3e65855d0128dce38e",
+    "url": "/static/media/LemonMilkbold.bb255cd0.otf"
+  },
+  {
+    "revision": "1d31288cc90a702efd2c7c05db3da175",
+    "url": "/static/media/Regulamento_intefrotas_go.1d31288c.pdf"
+  },
+  {
+    "revision": "36589485139932b7d331879a37d21f24",
+    "url": "/static/media/bg.36589485.jpg"
+  },
+  {
+    "revision": "c0b8d3ad8c293eec7068915c59cc4f00",
+    "url": "/static/media/logo-final.c0b8d3ad.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
